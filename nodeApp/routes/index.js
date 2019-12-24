ae0b111965b2549a32cf834f36068dce7ae3e09b
AndreyT <and6407@mail.ru>
2019-12-24 22:53:04 +0400

    project for notebook

--- nodeApp/routes/index.js
+++ nodeApp/routes/index.js
@@ -1,19 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const http = require("http");
-const port = 5000;
 
-const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end("Hello Node.js Server!");
-};
-const server = http.createServer(requestHandler);
-server.listen(port, err => {
-  if (err) {
-    return console.log("something bad happened", err);
-  }
-  console.log(`server is listening on ${port}`);
-});
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Express" });
